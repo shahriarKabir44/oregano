47031509e01f073c786e28e64ed27272e303c35e
Md. Shahriar Kabir <shahriar1904@cseku.ac.bd>
2022-04-10 03:18:25 +0600

    added pull to refresh

--- components/routed/NotificationsRoot.js
+++ components/routed/NotificationsRoot.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, RefreshControl } from 'react-native';
 import NotificationService from '../../services/NotificationServices';
 import NotificationItem from './notificationsViewUtils/NotificationItem';
 import { RootContext } from '../contexts/GlobalContext'
 import { useIsFocused } from '@react-navigation/native';
+
 function NotificationsRoot(props) {
     const isFocused = useIsFocused()
     let { updateContext, contextObject } = React.useContext(RootContext)
 
     const [notificationList, setNotificationList] = React.useState([])
+    const [refreshing, setRefreshing] = React.useState(false);
 
+    const onRefresh = React.useCallback(() => {
+        loadData()
 
+    }, []);
+    function loadData() {
+        setRefreshing(true)
+
+        updateContext({ ...contextObject, headerString: "Notifications" })
+        NotificationService.getNotifications(contextObject.currentUser.id)
+            .then(data => {
+                setNotificationList(data)
+            }).then(() => {
+                setRefreshing(false)
+            })
+    }
     React.useEffect(() => {
         if (isFocused) {
-            updateContext({ ...contextObject, headerString: "Notifications" })
-            NotificationService.getNotifications(contextObject.currentUser.id)
-                .then(data => {
-                    setNotificationList(data)
-                })
+            loadData()
         }
 
     }, [isFocused])
@@ -29,7 +41,10 @@ function NotificationsRoot(props) {
             borderRadius: 5,
             backgroundColor: "#ffffff"
         }}>
-            <ScrollView>
+            <ScrollView
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+
+            >
                 {notificationList.map((notification, index) => {
                     return <NotificationItem navigator={props.navigation} notificationItem={notification} key={index} />
                 })}
